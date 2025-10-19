228568cc87a3a909b0d3f85e33fd3ea85ed738e3
Mariah Stinson <mstinson@csumb.edu>
2025-10-19 14:43:22 -0700

    Initial Commit

---
+++ src/main/java/application/ControllerPrescriptionCreate.java
@@ -0,0 +1,8 @@
+package application;
+
+// TODO: Modify the controller classes to use the entity classes and repository.
+//  Remember to add instance variables for SequenceService and the repository interfaces.
+//  See ControllerDoctor for an example.
+
+public class ControllerPrescriptionCreate {
+}
